bd781e52ae2f17c92d970afdfb7912dd2db6520c
Tsuna-mi <belen_g_romero@hotmail.com>
2018-03-10 19:38:56 +0100

    Delete object destructuring

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -20,7 +20,7 @@ class DBHelper {
     xhr.onload = () => {
       if (xhr.status === 200) { // Got a success response from server!
         const json = JSON.parse(xhr.responseText);
-        const { restaurants } = json.restaurants;
+        const restaurants = json.restaurants;
         callback(null, restaurants);
       } else { // Oops!. Got an error from server.
         const error = (`Request failed. Returned status of ${xhr.status}`);
