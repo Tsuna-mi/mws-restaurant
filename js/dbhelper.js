c85f452ca835406e2ea23d4ad744d1fc809acc4b
Tsuna-mi <belen_g_romero@hotmail.com>
2018-03-10 23:05:34 +0100

    Fix load restaurant detail

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -39,7 +39,7 @@ class DBHelper {
       if (error) {
         callback(error, null);
       } else {
-        const restaurant = restaurants.find(r => r.id === id);
+        const restaurant = restaurants.find(r => r.id == id);
         if (restaurant) { // Got the restaurant
           callback(null, restaurant);
         } else { // Restaurant does not exist in the database
