470f90a45c592540ec95a02bd01428e91356969b
Tsuna-mi <belen_g_romero@hotmail.com>
2018-03-11 05:03:00 +0100

    Add new card element

--- js/main.js
+++ js/main.js
@@ -135,29 +135,33 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-
+  li.className = 'cards__item';
+  const card = document.createElement('div');
+  card.className = 'card';
+  li.append(card);
   const image = document.createElement('img');
-  image.className = 'restaurant-img';
+  image.className = 'card__img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   image.alt = `${restaurant.name}`;
-  li.append(image);
+  card.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  name.className = 'card__title';
+  card.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  card.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  card.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more);
+  card.append(more);
 
   return li;
 };
