7bc9b08a0cee6e7e5349e0ed9daa260245e60156
Tsuna-mi <belen_g_romero@hotmail.com>
2018-03-26 14:49:47 +0200

    Update gulptask to minify files, proper paths to files and serve dist with optimised files

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,45 @@
+// List Tasks:
+
+// serve (serve dev and watch files)
+// serve:dist (serve build files)
+// minify-js (js minify and sourcemaps)
+// minify-css (css minify and sourcemaps)
+// minify-html
+// minFiles (minify-js, minify-css and minify-html)
+// images (creates responsive jpg and png files)
+// webp (creates webp files from jpg files)
+
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const responsive = require('gulp-responsive');
 const webp = require('gulp-webp');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify-es').default;
+const minifycss = require('gulp-minify-css');
+const htmlmin = require('gulp-htmlmin');
+const util = require('gulp-util');
 
+// File where the favicon markups are stored
 const reload = browserSync.reload;
 
 const src = {
-  html: 'html/*.html',
-  css: 'assets/css/*.css',
-  js: 'assets/js/*.js'
+  dev: {
+    html: './*.html',
+    css: './assets/css/*.css',
+    js: './js/*.js',
+    img: './assets/img/'
+  },
+  dist: {
+    html: './dist/',
+    css: './dist/assets/css/',
+    js: './dist/js/',
+    img: './dist/assets/img/'
+  }
 };
-
-// process JS files and return the stream.
-// gulp.task('js', () => gulp.src('js/*js')
-//   .pipe(browserify())
-//   .pipe(uglify())
-//   .pipe(gulp.dest('dist/js')));
 
 
 // watch files for changes and reload
 gulp.task('serve', () => {
-  console.log(src.html);
   browserSync({
     port: 8000,
     injectChanges: true,
@@ -29,12 +48,58 @@ gulp.task('serve', () => {
     }
   });
 
-  gulp.watch([src.html, src.css, src.js]).on('change', reload);
+  gulp.watch([src.dev.html, src.dev.css, src.dev.js]).on('change', reload);
 });
+
+// watch files for changes and reload
+gulp.task('serve:dist', () => {
+  browserSync({
+    port: 8000,
+    injectChanges: false,
+    server: {
+      baseDir: './dist'
+    }
+  });
+  // copy data folder to dist folder
+  gulp.src(['assets/data/**/*']).pipe(gulp.dest('./dist/assets/data/'));
+  // copy manifest to dist folder
+  gulp.src(['./manifest.json']).pipe(gulp.dest('./dist/'));
+  gulp.watch([src.dev.html, src.dev.css, src.dev.js]).on('change', reload);
+});
+
+
+// minify-js
+gulp.task('minify-js', () => {
+  gulp.src(src.dev.js)
+    .pipe(sourcemaps.init({ identityMap: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./maps', { includeContent: false, sourceRoot: src.dev.js }))
+    .pipe(gulp.dest(src.dist.js));
+});
+
+
+// minify-css
+gulp.task('minify-css', () => {
+  gulp.src(src.dev.css)
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(minifycss())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest(src.dist.css));
+});
+
+// minify-html
+gulp.task('minify-html', () => {
+  gulp.src(src.dev.html)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(src.dist.html));
+});
+
+// Task for minify js(with sourcemaps), css and html
+gulp.task('minFiles', ['minify-js', 'minify-css', 'minify-html']);
 
 // generate responsive jpg files
 gulp.task('images', () => {
-  gulp.src('assets/img/*.jpg')
+  gulp.src(`${src.dev.img}*.{jpg,png}`)
     .pipe(responsive({
       // Resize all JPG images to three different sizes: 300, 400, and 600 pixels
       '*.jpg': [{
@@ -55,7 +120,14 @@ gulp.task('images', () => {
       }, {
         // Compress, strip metadata, and rename original image
         rename: { suffix: '-xl' }
-      }]
+      }],
+      '*.png': {
+        width: '100%'
+      },
+      '*': {
+        width: '100%'
+      }
+
     }, {
       // Global configuration for all images
       errorOnEnlargement: false,
@@ -67,12 +139,12 @@ gulp.task('images', () => {
       withMetadata: false,
       max: true
     }))
-    .pipe(gulp.dest('public/img'));
+    .pipe(gulp.dest(src.dist.img));
 });
 
 // generate webp from responsive jpg files
 gulp.task('webp', () => {
-  gulp.src('public/img/*.jpg')
+  gulp.src(`${src.dev.img}*.jpg`)
     .pipe(webp())
-    .pipe(gulp.dest('public/img'));
+    .pipe(gulp.dest(src.dist.img));
 });
