40e0d0d60f55ecd28d6d21194274fef03db91ba8
Tsuna-mi <belen_g_romero@hotmail.com>
2018-03-30 17:20:13 +0200

    Add register sw at load event

--- js/main.js
+++ js/main.js
@@ -10,30 +10,32 @@ var markers = [];
  */
 
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js', { scope: './' })
-    .then((registration) => {
-      console.log('SW registered!');
-      let sw;
-      if (registration.installing) {
-        sw = registration.installing;
-        console.log('SW installing!');
-      } else if (registration.waiting) {
-        sw = registration.waiting;
-        console.log('SW installed and waiting!');
-      } else if (registration.active) {
-        sw = registration.active;
-        console.log('SW active!');
-      }
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js', { scope: './' })
+      .then((registration) => {
+        console.log('SW registered!');
+        let sw;
+        if (registration.installing) {
+          sw = registration.installing;
+          console.log('SW installing!');
+        } else if (registration.waiting) {
+          sw = registration.waiting;
+          console.log('SW installed and waiting!');
+        } else if (registration.active) {
+          sw = registration.active;
+          console.log('SW active!');
+        }
 
-      if (sw) {
-        console.log(sw.state);
-        sw.addEventListener('statechange', (e) => {
-          console.log(e.target.state);
-        });
-      }
-    }).catch(() => {
-      console.log('SW Registration failed!');
-    });
+        if (sw) {
+          console.log(sw.state);
+          sw.addEventListener('statechange', (e) => {
+            console.log(e.target.state);
+          });
+        }
+      }).catch(() => {
+        console.log('SW Registration failed!');
+      });
+  });
 }
 
 
