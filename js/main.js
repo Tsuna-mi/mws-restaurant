5768870a6944b8b567377f5293952430b48364dd
Tsuna-mi <belen_g_romero@hotmail.com>
2018-03-31 12:21:25 +0200

    Extract logic to register ServiceWorker at entries

--- js/main.js
+++ js/main.js
@@ -6,40 +6,6 @@ var markers = [];
 
 
 /**
- * Register sw.
- */
-
-if (navigator.serviceWorker) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', { scope: './' })
-      .then((registration) => {
-        console.log('SW registered!');
-        let sw;
-        if (registration.installing) {
-          sw = registration.installing;
-          console.log('SW installing!');
-        } else if (registration.waiting) {
-          sw = registration.waiting;
-          console.log('SW installed and waiting!');
-        } else if (registration.active) {
-          sw = registration.active;
-          console.log('SW active!');
-        }
-
-        if (sw) {
-          console.log(sw.state);
-          sw.addEventListener('statechange', (e) => {
-            console.log(e.target.state);
-          });
-        }
-      }).catch(() => {
-        console.log('SW Registration failed!');
-      });
-  });
-}
-
-
-/**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
